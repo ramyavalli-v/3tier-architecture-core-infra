c5605c8c4edac165f4673794858079e2656b05c0
ramyavalli-v <ramyavalli2509@gmail.com>
2026-04-21 23:15:33 +0530

    output file update

--- outputs.tf
+++ outputs.tf
@@ -5,22 +5,22 @@ output "vpc_id" {
 
 output "public_subnet_ids" {
   description = "Public subnet IDs."
-  value       = module.network.public_subnet_ids
+  value       = module.network.public[*].id
 }
 
 output "private_web_subnet_ids" {
   description = "Private web tier subnet IDs."
-  value       = module.network.private_web_subnet_ids
+  value       = module.network.private_web[*].id
 }
 
 output "private_app_subnet_ids" {
   description = "Private app tier subnet IDs."
-  value       = module.network.private_app_subnet_ids
+  value       = module.network.private_app[*].id
 }
 
 output "private_db_subnet_ids" {
   description = "Private database subnet IDs."
-  value       = module.network.private_db_subnet_ids
+  value       = module.network.private_db[*].id
 }
 
 output "security_group_ids" {
